8c4fd7a6ad6a16603243717b5461b72c1aaf3cff
Ozsumit <oniwagon>
2024-10-01 21:56:57 +0545

    dry

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-import withPWA from "next-pwa";
-
-const config = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  output: "export",
 };
 
-const nextConfig = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  // Remove the sw property for now
-  // sw: "/service-worker.ts",
-})(config);
-
-export default nextConfig;
+module.exports = nextConfig;
